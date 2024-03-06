545222755e841ce2f328390a7afd71853bac3e40
Saif Iqbal Ghouri <160517769+Techgithubr@users.noreply.github.com>
2024-03-06 15:17:22 -0800

    first commit

---
+++ biodata.js
@@ -0,0 +1,78 @@
+// Persnol information:
+var fullName = "ASIM IQBAL GHOURI";
+var fatherName = "IQBAL RASHEED GHOURI";
+var age = 20;
+var isGender = "Male";
+var isStudent = false;
+var isEmployed = true;
+var isMarried = false;
+var contact = "0316-6879391";
+var email = "asimiqbalg098@gmail.com";
+var religion = "Islam";
+// Adress:
+var address = "House A/58 dalmia shanti nagar karachi";
+var area = "Dalmia";
+var city = "Karachi";
+var country = "Pakistan";
+// skills:
+var skills = "Ms Office,Artificial Intellegence(AI),Metaverse & Web 3.0 Devoloper (In Process)";
+// Language:
+var lang = "English,Urdu";
+// Education:
+var schoolName = "Gulshan-e-Iqbal Secondary School (GISS)";
+var board = "Board Of Secondary Education Karachi (BSEK)";
+var group = "Science";
+var degree = "Matric";
+var year = "2018";
+var collegename = "Vocational Training Institute College (VTI)";
+var board1 = "Technical Board Of Education Karachi";
+var group1 = "Mechanical Engineering";
+var Degree = "Diploma Association of Engineering";
+var year1 = "2021";
+// Experience:
+var experience = "Hardware & Sanitary Store";
+var position = "Incharge";
+var year2 = "2022-2024";
+var experience1 = "Eatzo Fast Food";
+var position1 = "Self Employed";
+var year3 = "In Present";
+//Printing Biodata:
+console.log("PERSNOL INFORMATION:-");
+console.log("Name:", fullName);
+console.log("Father Name:", fatherName);
+console.log("Age:", age);
+console.log("Gender:", isGender);
+console.log("Student:", isStudent);
+console.log("Employed:", isEmployed);
+console.log("Married:", isMarried);
+console.log("Contact:", contact);
+console.log("E-Mail:", email);
+console.log("Religion:", religion);
+console.log("ADDRESS:-");
+console.log("Area:", area);
+console.log("City:", city);
+console.log("Country:", country);
+console.log("Adress:", address);
+console.log("SKILLS:-");
+console.log("Skill:", skills);
+console.log("LANGUAGES:-");
+console.log("Language:", lang);
+console.log("MATRICULATION:-");
+console.log("School:", schoolName);
+console.log("Board:", board);
+console.log("Group:", group);
+console.log("Degree:", degree);
+console.log("Year:", year);
+console.log("DAE:-");
+console.log("College:", collegename);
+console.log("Board:", board1);
+console.log("Group:", group1);
+console.log("Degree:", Degree);
+console.log("Year:", year);
+console.log("EXPERIENCE:-");
+console.log("Experience:", experience);
+console.log("Position:", position);
+console.log("Year:", year2);
+console.log("Experience:", experience1);
+console.log("Position:", position1);
+console.log("Year:", year3);
---
+++ biodata.ts
@@ -0,0 +1,86 @@
+// Persnol information:
+let fullName: string = "ASIM IQBAL GHOURI"
+let fatherName: string = "IQBAL RASHEED GHOURI"
+let age: number = 20
+let isGender: string = "Male"
+let isStudent: boolean = false
+let isEmployed: boolean = true
+let isMarried: boolean = false 
+const contact: string = "0316-6879391"
+const email: string = "asimiqbalg098@gmail.com"
+const religion: string = "Islam"
+
+// Adress:
+let address: string = "House A/58 dalmia shanti nagar karachi"
+let area: string ="Dalmia"
+let city: string = "Karachi"
+let country: string = "Pakistan"
+
+// skills:
+const skills: string = "Ms Office,Artificial Intellegence(AI),Metaverse & Web 3.0 Devoloper (In Process)"
+
+// Language:
+const lang: string ="English,Urdu"
+
+// Education:
+const schoolName: string = "Gulshan-e-Iqbal Secondary School (GISS)"
+const board: string = "Board Of Secondary Education Karachi (BSEK)"
+const group: string = "Science"
+const degree: string = "Matric"
+const year: string = "2018"
+const collegename: string = "Vocational Training Institute College (VTI)"
+const board1: string = "Technical Board Of Education Karachi"
+const group1: string = "Mechanical Engineering"
+const Degree: string = "Diploma Association of Engineering"
+const year1: string = "2021"
+
+// Experience:
+let experience: string = "Hardware & Sanitary Store"
+let position: string = "Incharge"
+let year2: string = "2022-2024"
+let experience1: string = "Eatzo Fast Food"
+let position1: string = "Self Employed"
+let year3: string = "In Present"
+
+//Printing Biodata:
+console.log("PERSNOL INFORMATION:-")
+console.log("Name:",fullName)
+console.log("Father Name:",fatherName)
+console.log("Age:",age)
+console.log("Gender:",isGender)
+console.log("Student:",isStudent)
+console.log("Employed:",isEmployed)
+console.log("Married:",isMarried)
+console.log("Contact:",contact)
+console.log("E-Mail:",email)
+console.log("Religion:",religion)
+console.log("ADDRESS:-")
+console.log("Area:",area)
+console.log("City:",city)
+console.log("Country:",country)
+console.log("Adress:",address)
+console.log("SKILLS:-")
+console.log("Skill:",skills)
+console.log("LANGUAGES:-")
+console.log("Language:",lang)
+console.log("MATRICULATION:-")
+console.log("School:",schoolName)
+console.log("Board:",board)
+console.log("Group:",group)
+console.log("Degree:",degree)
+console.log("Year:",year)
+console.log("DAE:-")
+console.log("College:",collegename)
+console.log("Board:",board1)
+console.log("Group:",group1)
+console.log("Degree:",Degree)
+console.log("Year:",year)
+console.log("EXPERIENCE:-")
+console.log("Experience:",experience)
+console.log("Position:",position)
+console.log("Year:",year2)
+console.log("Experience:",experience1)
+console.log("Position:",position1)
+console.log("Year:",year3)
+
+
